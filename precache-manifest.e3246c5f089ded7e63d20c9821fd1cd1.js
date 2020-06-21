bf6b3ff933fcd3b1544171a458fada47a8f43609
abelfubu <abelfubu@gmail.com>
2020-06-21 15:36:03 +0200

    Updates

---
+++ precache-manifest.e3246c5f089ded7e63d20c9821fd1cd1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f325d2696eecf5e0f7368ff6b06691e",
+    "url": "/obj-/index.html"
+  },
+  {
+    "revision": "ee815c485cc89ec730f8",
+    "url": "/obj-/static/css/main.d9191b76.chunk.css"
+  },
+  {
+    "revision": "712e9d80d93555f2a72d",
+    "url": "/obj-/static/js/2.a31370c3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/obj-/static/js/2.a31370c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee815c485cc89ec730f8",
+    "url": "/obj-/static/js/main.df7162cf.chunk.js"
+  },
+  {
+    "revision": "6ac46633439ad8290687",
+    "url": "/obj-/static/js/runtime-main.61f032cb.js"
+  },
+  {
+    "revision": "9d27be5d205b65f5392755408ed6e51b",
+    "url": "/obj-/static/media/Fondo.9d27be5d.png"
+  },
+  {
+    "revision": "0e47723824b4ea2f52d3aa49a448ecdd",
+    "url": "/obj-/static/media/FondoDark.0e477238.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/obj-/precache-manifest.cfe312b791586deabc8cb017f774f599.js"
+  "/obj-/precache-manifest.e3246c5f089ded7e63d20c9821fd1cd1.js"
 );
 
 self.addEventListener('message', (event) => {
